9a83d3073b71f99185e1d954a0731e496b755118
Gago55 <xgagik8@gmail.com>
2021-06-08 20:45:31 +0400

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React from 'react'
+import { Box, Container, createMuiTheme, Paper, ThemeProvider, Typography } from '@material-ui/core';
+import Header from './Header';
+import Contracts from './Contracts';
+import Contract from './Contract';
+import Codes from './Codes';
+import { Route, RouteComponentProps } from 'react-router';
 
-function App() {
+interface MatchParams {
+  address?: string;
+}
+
+interface MatchProps extends RouteComponentProps<MatchParams> {
+}
+
+const App = () => {
+
   return (
     <div className="App">
-      Secret Contracts
+      <Header />
+      <Container>
+        <Box m={3} style={{}}>
+          <Route exact path="/" render={() => <Codes />} />
+          <Route path="/:address?" render={({ match }: MatchProps) => <Contract address={match.params.address} />} />
+        </Box>
+      </Container>
     </div>
-  );
+  )
 }
 
 export default App;
