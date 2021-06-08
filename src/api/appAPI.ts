1ad437ba879b1bce71d6ae6c56ebe60f7bff784d
Gago55 <xgagik8@gmail.com>
2021-06-08 20:43:43 +0400

    Add appReducer, api methods

--- src/api/appAPI.ts
+++ src/api/appAPI.ts
@@ -1,33 +1,38 @@
 import { secretContracts } from "./axiosInstances";
 
-type ContractType = {
+export type ContractType = {
     address: string
     codeId: number
     label: string
     creator: string
 }
 
-type CodeType = {
+export type CodeType = {
     id: number
     isVerified: boolean
     creator: string
     checksum: string
     source?: string
     builder?: 'enigmampc/secret-contract-optimizer:1.0.0' | 'enigmampc/secret-contract-optimizer:1.0.1' | 'enigmampc/secret-contract-optimizer:1.0.2' | 'enigmampc/secret-contract-optimizer:1.0.3' | 'enigmampc/secret-contract-optimizer:1.0.4'
+    contracts: Array<ContractType>
 }
 
-export const getContractByAddress = (address: string) => (
-    secretContracts.get<ContractType>(`/contracts/${address}`)
+export const fetchContractByAddress = (address: string) => (
+    secretContracts.get<ContractType>(`/contracts/${address}`).then(res => res.data)
 )
 
-export const getContractsByCodeID = (codeID: string | number) => (
-    secretContracts.get<Array<ContractType>>(`/contracts/codeID/${codeID}`)
+export const fetchContractsByCodeId = (codeId: string | number) => (
+    secretContracts.get<Array<ContractType>>(`/contracts/codeId/${codeId}`).then(res => res.data)
 )
 
-export const getCode = (id: string | number) => (
-    secretContracts.get<CodeType>(`/codes/${id}`)
+export const fetchCode = (id: string | number) => (
+    secretContracts.get<CodeType>(`/codes/${id}`).then(res => res.data)
 )
 
-export const getCodes = () => (
-    secretContracts.get<Array<CodeType>>(`/codes`)
+export const fetchCodes = () => (
+    secretContracts.get<Array<CodeType>>(`/codeswithcontracts`).then(res => res.data)
+)
+
+export const fetchCodeByContractAddress = (address: string) => (
+    secretContracts.get<CodeType>(`/codeswithcontracts/contractAddress/${address}`).then(res => res.data)
 )
--- src/redux/appReducer.ts
+++ src/redux/appReducer.ts
@@ -1,21 +1,82 @@
-import { InferActionsType } from './store';
+import { Dispatch } from 'redux';
+import { ThunkAction } from 'redux-thunk';
+import { CodeType, fetchCodeByContractAddress, fetchCodes } from './../api/appAPI';
+import { InferActionsType, StateType } from './store';
 
 const initState = {
+    codes: [] as Array<CodeType>,
+    actualCode: undefined as undefined | CodeType
 }
 
 type InitStateType = typeof initState
 
-const threeReducer = (state = initState, action: ActionsType): InitStateType => {
-    // switch (action.type) {
-    //     default:
-    return state
-    // }
+const appReducer = (state = initState, action: ActionsType): InitStateType => {
+    switch (action.type) {
+        case 'sc/app/SET_CODES':
+            return {
+                ...state,
+                codes: [...action.arr]
+            }
+        case 'sc/app/SET_ACTUAL_CODE':
+            return {
+                ...state,
+                actualCode: action.code
+            }
+        case 'sc/app/SET_ACTUAL_CODE_BY_ID':
+            return {
+                ...state,
+                actualCode: state.codes.find(code => code.id == action.codeId)
+            }
+        default:
+            return state
+    }
 }
 
 export const actions = {
-
+    setCodes: (arr: Array<CodeType>) => ({ type: 'sc/app/SET_CODES', arr } as const),
+    setActualCode: (code: CodeType) => ({ type: 'sc/app/SET_ACTUAL_CODE', code } as const),
+    setActualCodeById: (codeId: number) => ({ type: 'sc/app/SET_ACTUAL_CODE_BY_ID', codeId } as const)
 }
 
 export type ActionsType = InferActionsType<typeof actions>
 
-export default threeReducer
+type ThunkType = ThunkAction<Promise<void>, StateType, unknown, ActionsType>
+type DispatchType = Dispatch<ActionsType>
+
+//Thunks
+export const getCodes = (): ThunkType => async (dispatch: DispatchType) => {
+    try {
+        const codes = await fetchCodes()
+        dispatch(actions.setCodes(codes))
+    } catch (error) {
+
+    }
+}
+
+export const getCodeByContractAddress = (address: string): ThunkType => async (dispatch: DispatchType, getState: () => StateType) => {
+
+    const codes = getState().appReducer.codes
+
+    let codeId = null as null | number
+
+    for (const code of codes) {
+        code.contracts.forEach(c => {
+            if (c.address === address)
+                codeId = code.id
+        })
+    }
+
+    if (codeId !== null) {
+        dispatch(actions.setActualCodeById(codeId))
+    }
+    else {
+        try {
+            const code = await fetchCodeByContractAddress(address)
+            dispatch(actions.setActualCode(code))
+        } catch (error) {
+
+        }
+    }
+}
+
+export default appReducer
