707ccfbe07a895f1f85128594d5aa3d0f3a65c22
Gago55 <xgagik8@gmail.com>
2021-06-09 23:04:29 +0400

    Add sort feature

--- src/components/Codes.tsx
+++ src/components/Codes.tsx
@@ -1,12 +1,12 @@
-import { Box, Button, Collapse, makeStyles, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@material-ui/core'
+import { Box, Button, Collapse, makeStyles, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TableSortLabel, Typography } from '@material-ui/core'
 import IconButton from '@material-ui/core/IconButton'
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown'
 import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp'
-import React, { FC, useCallback, useEffect, useState } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import { connect } from 'react-redux'
-import { NavLink, useHistory } from 'react-router-dom'
-import { CodeType } from '../api/appAPI'
-import { getCodes, actions } from '../redux/appReducer'
+import { useHistory } from 'react-router-dom'
+import { CodeType, ContractType } from '../api/appAPI'
+import { actions, getCodes } from '../redux/appReducer'
 import { StateType } from '../redux/store'
 
 interface IProps {
@@ -17,26 +17,72 @@ interface IProps {
 }
 
 interface IRowProps {
-    row: CodeType
+    row: RowDataType
+    contracts: Array<ContractType>
 
     setActualCodeById(codeId: number): void
 }
+
+interface ISortedTableHeadProps {
+    classes: ReturnType<typeof useStyles>
+    onRequestSort: (
+        event: React.MouseEvent<unknown>,
+        property: OrderByType
+    ) => void
+    order: Order
+    orderBy: string
+}
+
+interface HeadCellType {
+    id: OrderByType
+    label: string
+    alignRight: boolean
+}
+
+interface RowDataType {
+    id: number
+    contractsCount: number
+    isVerified: string
+    contracts: Array<ContractType>
+}
+
+type Order = "asc" | "desc"
+type OrderByType = 'id' | 'contractsCount' | 'isVerified'
+
+type GetComparatorReturnType = (
+    a: { [key in OrderByType]: number | string },
+    b: { [key in OrderByType]: number | string }
+) => number
+
 
 const useRowStyles = makeStyles({
     root: {
         '& > *': {
             borderBottom: 'unset',
         },
-
     },
-});
+})
 
-const Row: FC<IRowProps> = ({ row, setActualCodeById }) => {
-    const classes = useRowStyles();
+const useStyles = makeStyles({
+    visuallyHidden: {
+        border: 0,
+        clip: "rect(0 0 0 0)",
+        height: 1,
+        margin: -1,
+        overflow: "hidden",
+        padding: 0,
+        position: "absolute",
+        top: 20,
+        width: 1
+    }
+})
+
+const Row: FC<IRowProps> = ({ row, contracts, setActualCodeById }) => {
+    const classes = useRowStyles()
 
     const [open, setOpen] = useState(false)
 
-    const history = useHistory();
+    const history = useHistory()
     const handleOnClick = (address: string) => history.push('/' + address)
 
     return (
@@ -47,14 +93,14 @@ const Row: FC<IRowProps> = ({ row, setActualCodeById }) => {
                         {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
                     </IconButton>
                 </TableCell>
-                <TableCell align='center' component="th" scope="row">
+                <TableCell component="th" scope="row">
                     {row.id}
                 </TableCell>
-                <TableCell align='center' >{row.contracts.length}</TableCell>
-                <TableCell align='center' >{row.isVerified.toString()}</TableCell>
+                <TableCell align='right'>{row.contractsCount}</TableCell>
+                <TableCell align='right'>{row.isVerified}</TableCell>
                 <TableCell align='center' >
-                    {row.isVerified && <Button>Show</Button>}
-                    {!row.isVerified && <Button>Provide</Button>}
+                    {row.isVerified == 'true' && <Button variant='contained' color='primary'>Show</Button>}
+                    {row.isVerified == 'false' && <Button variant='contained' color='primary'>Provide</Button>}
                 </TableCell>
             </TableRow>
             <TableRow>
@@ -64,7 +110,6 @@ const Row: FC<IRowProps> = ({ row, setActualCodeById }) => {
                             <Typography variant="h6" gutterBottom component="div">
                                 Contracts
                             </Typography>
-                            {/* <TableContainer component={Paper}> */}
                             <Paper elevation={2}>
                                 <Table size="small" aria-label="purchases">
                                     <TableHead>
@@ -74,8 +119,7 @@ const Row: FC<IRowProps> = ({ row, setActualCodeById }) => {
                                         </TableRow>
                                     </TableHead>
                                     <TableBody>
-                                        {row.contracts.map((contract) => (
-                                            // <NavLink to={`/` + contract.address} >
+                                        {contracts.map((contract) => (
                                             <TableRow key={contract.address} hover onClick={() => {
                                                 setActualCodeById(row.id)
                                                 handleOnClick(contract.address)
@@ -85,12 +129,10 @@ const Row: FC<IRowProps> = ({ row, setActualCodeById }) => {
                                                 </TableCell>
                                                 <TableCell>{contract.label}</TableCell>
                                             </TableRow>
-                                            // </NavLink>
                                         ))}
                                     </TableBody>
                                 </Table>
                             </Paper>
-                            {/* </TableContainer> */}
                         </Box>
                     </Collapse>
                 </TableCell>
@@ -99,31 +141,138 @@ const Row: FC<IRowProps> = ({ row, setActualCodeById }) => {
     )
 }
 
+function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+    if (b[orderBy] < a[orderBy]) {
+        return -1
+    }
+    if (b[orderBy] > a[orderBy]) {
+        return 1
+    }
+    return 0
+}
+
+const getComparator = (order: Order, orderBy: OrderByType): GetComparatorReturnType => {
+    return order === "desc"
+        ? (a, b) => descendingComparator(a, b, orderBy)
+        : (a, b) => -descendingComparator(a, b, orderBy)
+}
+
+function stableSort<T>(array: T[], comparator: (a: T, b: T) => number) {
+    const stabilizedThis = array.map((el, index) => [el, index] as [T, number])
+    stabilizedThis.sort((a, b) => {
+        const order = comparator(a[0], b[0])
+        if (order !== 0) return order
+        return a[1] - b[1]
+    })
+    return stabilizedThis.map((el) => el[0])
+}
+
+const headCells = [
+    {
+        id: "id",
+        alignRight: false,
+        label: "ID"
+    },
+    {
+        id: "contractsCount",
+        alignRight: true,
+        label: "Contracts Count"
+    },
+    {
+        id: "isVerified",
+        alignRight: true,
+        label: "Is Verified"
+    }
+] as Array<HeadCellType>
+
+const SortedTableHead: FC<ISortedTableHeadProps> = ({ classes, order, orderBy, onRequestSort }) => {
+
+    const createSortHandler = (property: OrderByType) => (event: React.MouseEvent<unknown>) => { onRequestSort(event, property) }
+
+    return (
+        <TableHead>
+            <TableRow>
+                <TableCell />
+                {headCells.map((headCell) => (
+                    <TableCell
+                        key={headCell.id}
+                        align={headCell.alignRight ? 'right' : 'left'}
+                        sortDirection={orderBy === headCell.id ? order : false}
+                    >
+                        <TableSortLabel
+                            active={orderBy === headCell.id}
+                            direction={orderBy === headCell.id ? order : "asc"}
+                            onClick={createSortHandler(headCell.id)}
+                        >
+                            {headCell.label}
+                            {orderBy === headCell.id ? (
+                                <span className={classes.visuallyHidden}>
+                                    {order === "desc" ? "sorted descending" : "sorted ascending"}
+                                </span>
+                            ) : null}
+                        </TableSortLabel>
+                    </TableCell>
+                ))}
+                <TableCell align='center' >Action</TableCell>
+            </TableRow>
+        </TableHead>
+    )
+}
+
 const Codes: FC<IProps> = props => {
+    const classes = useStyles()
+
+    const [order, setOrder] = React.useState<Order>("asc")
+    const [orderBy, setOrderBy] = React.useState<OrderByType>("id")
+    const [rows, setRows] = useState<Array<RowDataType>>([])
+
+    const handleRequestSort = (event: React.MouseEvent<unknown>, property: OrderByType) => {
+        const isAsc = orderBy === property && order === "asc"
+        setOrder(isAsc ? "desc" : "asc")
+        setOrderBy(property)
+
+        setRows(stableSort(rows, getComparator(isAsc ? "desc" : "asc", property)))
+    }
 
     useEffect(() => {
-        props.getCodes()
-    }, [])
+        if (!props.codes.length)
+            props.getCodes()
+        else {
+            const newRows = [] as Array<RowDataType>
+
+            props.codes.forEach(code => newRows.push({
+                id: code.id,
+                contractsCount: code.contracts.length,
+                isVerified: code.isVerified.toString(),
+                contracts: code.contracts
+            }))
+
+            setRows(newRows)
+        }
+
+    }, [props.codes])
 
     return (
         <>
             <Typography variant="h6" noWrap style={{ marginBottom: 5 }}>
                 Codes
-          </Typography>
+            </Typography>
             <TableContainer component={Paper}>
-                <Table aria-label="collapsible table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell />
-                            <TableCell align='center'>ID</TableCell>
-                            <TableCell align='center'>Contracts count</TableCell>
-                            <TableCell align='center'>Is Verified</TableCell>
-                            <TableCell align='center'>Action</TableCell>
-                        </TableRow>
-                    </TableHead>
+                <Table>
+                    <SortedTableHead
+                        classes={classes}
+                        order={order}
+                        orderBy={orderBy}
+                        onRequestSort={handleRequestSort}
+                    />
                     <TableBody>
-                        {props.codes.map((row: CodeType) => (
-                            <Row key={row.id} row={row} setActualCodeById={props.setActualCodeById} />
+                        {rows.map((row, i) => (
+                            <Row
+                                key={row.id}
+                                row={row}
+                                contracts={row.contracts}
+                                setActualCodeById={props.setActualCodeById}
+                            />
                         ))}
                     </TableBody>
                 </Table>
